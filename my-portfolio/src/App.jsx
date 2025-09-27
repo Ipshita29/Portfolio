03bbdd91cf383348f11a0e40f8aa1573fe19b821
Ipshita Patel <ipshita2908@gmail.com>
2025-09-27 17:15:10 +0530

    Delete my-portfolio directory

--- my-portfolio/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Portfolio from "./Portfolio"; // Import Portfolio component
-import "./App.css"; // Import CSS file
-
-function App() {
-  return <Portfolio />;
-}
-
-export default App;
